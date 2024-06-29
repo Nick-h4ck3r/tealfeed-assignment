6c4a08d3e098bf5faf2afebb74f7a3faa9265fca
Nikhil Kadam <nickkadam23@gmail.com>
2024-06-29 21:29:59 +0530

    feat: add line numbers

--- components/CodeEditor.tsx
+++ components/CodeEditor.tsx
@@ -48,16 +48,17 @@ const CodeEditor = () => {
             style={{ ...style, minHeight: "24rem" }}
           >
             {tokens.map((line, i) => (
-              <div
-                key={i}
-                {...getLineProps({ line, key: i })}
-              >
-                {line.map((token, key) => (
-                  <span
-                    key={key}
-                    {...getTokenProps({ token, key })}
-                  />
-                ))}
+              <div key={i} className="table-row">
+                <span className="table-cell text-right pr-4 select-none opacity-50 text-sm">
+                  {i + 1}
+                </span>
+                <span className="table-cell">
+                  <div {...getLineProps({ line, key: i })}>
+                    {line.map((token, key) => (
+                      <span key={key} {...getTokenProps({ token, key })} />
+                    ))}
+                  </div>
+                </span>
               </div>
             ))}
           </pre>
@@ -69,7 +70,7 @@ const CodeEditor = () => {
         value={code}
         onChange={handleChange}
         onScroll={handleScroll}
-        className="absolute top-0 left-0 w-full h-full p-4 m-0 bg-transparent resize-none outline-none font-mono text-transparent caret-white overflow-auto whitespace-pre-wrap break-words"
+        className="absolute top-0 left-0 w-full h-full p-4 m-0 bg-transparent resize-none outline-none font-mono text-transparent caret-white overflow-auto whitespace-pre-wrap break-words pl-12"
         spellCheck="false"
         style={{ minHeight: "24rem" }}
       />
